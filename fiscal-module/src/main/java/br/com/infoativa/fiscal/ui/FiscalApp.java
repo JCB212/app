6306e08de44fe56a0698b7ee1141c5cdce098209
Jean Carlos de Jesus Barreto <134634600+JCB212@users.noreply.github.com>
2026-04-17 12:12:59 -0300

    feat: menu Historico/Cache + HistoricoApp integrado

--- fiscal-module/src/main/java/br/com/infoativa/fiscal/ui/FiscalApp.java
+++ fiscal-module/src/main/java/br/com/infoativa/fiscal/ui/FiscalApp.java
@@ -235,9 +235,10 @@ public class FiscalApp extends Application {
         Button btnReproc  = menuBtn("Reprocessar Mês",     ICON_REFRESH, "reprocess");
         Button btnDest    = menuBtn("Destinatários",       ICON_EMAIL,   "dest");
         Button btnEmail   = menuBtn("Config. E-mail",      ICON_SHIELD,  "email");
+        Button btnHist    = menuBtn("Histórico/Cache",      ICON_HIST,    "historico");
         Button btnConfig  = menuBtn("Configurações",       ICON_SETTINGS,"config");
 
-        menu.getChildren().addAll(btnHome, btnProcess, btnReproc, btnDest, btnEmail, btnConfig);
+        menu.getChildren().addAll(btnHome, btnProcess, btnReproc, btnDest, btnEmail, btnHist, btnConfig);
 
         // Painel de status DB
         VBox dbPanel = new VBox(5);
@@ -310,6 +311,7 @@ public class FiscalApp extends Application {
             case "dest"      -> showDestinatarios();
             case "email"     -> showEmailConfig();
             case "config"    -> showConfig();
+            case "historico" -> showHistorico();
         }
     }
 
@@ -1020,5 +1022,20 @@ public class FiscalApp extends Application {
     private static final String ICON_REFRESH  = "M17.65,6.35C16.2,4.9,14.21,4,12,4A8,8,0,1,0,20,12h-2A6,6,0,1,1,12,6a5.92,5.92,0,0,1,4.22,1.78L13,11h7V4Z";
     private static final String ICON_EMAIL    = "M20,4H4C2.9,4,2,4.9,2,6v12c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V6C22,4.9,21.1,4,20,4z M20,8l-8,5L4,8V6l8,5l8-5V8z";
     private static final String ICON_SHIELD   = "M12,1L3,5v6c0,5.55,3.84,10.74,9,12c5.16-1.26,9-6.45,9-12V5L12,1z";
+    private static final String ICON_HIST     = "M12,3C7.58,3,4,4.79,4,7C4,9.21,7.58,11,12,11C16.42,11,20,9.21,20,7C20,4.79,16.42,3,12,3M4,9V12C4,14.21,7.58,16,12,16C16.42,16,20,14.21,20,12V9C20,11.21,16.42,13,12,13C7.58,13,4,11.21,4,9M4,14V17C4,19.21,7.58,21,12,21C16.42,21,20,19.21,20,17V14C20,16.21,16.42,18,12,18C7.58,18,4,16.21,4,14Z";
     private static final String ICON_SETTINGS = "M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.43-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z";
+
+    private void showHistorico() {
+        try {
+            ensureDbConnected();
+            HistoricoApp historico = new HistoricoApp(dbGateway.getConnection());
+            contentArea.getChildren().setAll(historico);
+            historico.pesquisar();
+        } catch (Exception e) {
+            log.error("Erro ao abrir histórico", e);
+            showAlert("Erro ao carregar histórico: " + e.getMessage() +
+                "\n\nVerifique a conexão com o banco de dados.", Alert.AlertType.ERROR);
+        }
+    }
+
 }
